38bd8b490b1330b695ec064ceeb818ea305f0542
Giacomo <gcasoni@hotmail.it>
2020-06-18 13:37:43 +0200

    NEXT HOP

--- evpn_parser.py
+++ evpn_parser.py
@@ -158,7 +158,7 @@ bmp_message_types = {
 }
 
 single_length_path_attributes = {
-    "ORIGIN", "EXTENDED COMMUNITIES", "MULTI_EXIT_DISC", "COMMUNITY"}
+    "ORIGIN", "EXTENDED COMMUNITIES", "MULTI_EXIT_DISC", "COMMUNITY", "NEXT_HOP"}
 double_length_path_attributes = {
     "MP_REACH_NLRI", "AS_PATH", "MP_UNREACH_NLRI"}
 
